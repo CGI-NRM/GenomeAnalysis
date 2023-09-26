aab087ca196c8ac1e0b0a3943fc6f53652b30b02
wfenglund <72128748+wfenglund@users.noreply.github.com>
2023-09-26 16:49:13 +0200

    Create kmergenie_script.sh

---
+++ Project_clone/Denovo/Kmergenie/kmergenie_script.sh
@@ -0,0 +1,20 @@
+#! /bin/bash -l 
+#SBATCH -p node
+#SBATCH -n 1
+#SBATCH -C mem256GB
+#SBATCH -t 10:00:00
+#SBATCH -J kmer_estimation
+
+# navigate to the correct directory:
+cd $PROJECT_DIR
+cd Denovo/Kmergenie/
+
+# load software modules:
+module load bioinfo-tools
+module load KmerGenie
+
+# find fastq-files:
+ls -1 ../../Filtered_data/*fq.gz > fastq_files.txt
+
+# run kmergenie:
+kmergenie fastq_files.txt
