243d7925eadf2450a61f591d32ee104394e45fa4
wfenglund <72128748+wfenglund@users.noreply.github.com>
2023-11-28 16:06:55 +0100

    Update set_variables.sh

--- Project_clone/set_variables.sh
+++ Project_clone/set_variables.sh
@@ -1,5 +1,5 @@
 # Set the UPPMAX project that will be used when submitting SLURM-jobs:
-export PROJECT_ID="naiss202X-XX-XX"
+export SBATCH_ACCOUNT="naiss202X-XX-XX"
 # Set the name of the organism of study:
 export ORGANISM="harr"
 # Set the location of this file (The top of the project directory):
