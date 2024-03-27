d9f5ae989449d3a601c9fe9165e74cfe6d4aa3f8
wfenglund <72128748+wfenglund@users.noreply.github.com>
2024-03-27 14:48:42 +0100

    fix so that populations correspond to popfile.txt

--- Project_clone/Tools/Admixture/plot_admixture.sh
+++ Project_clone/Tools/Admixture/plot_admixture.sh
@@ -5,4 +5,4 @@ module load R_packages
 wget https://raw.githubusercontent.com/speciationgenomics/scripts/master/plotADMIXTURE.r
 
 # Run script, make sure that you have prepared a popfile.txt corresponding to "-l":
-Rscript ../plotADMIXTURE.r -p pphocoena_1 -i popfile.txt -k 5 -l Ostersjon,Skagerrak,Kattegatt,Oresund,Unknown
+Rscript ./plotADMIXTURE.r -p pphocoena_1 -i popfile.txt -k 5 -l population1,population2
